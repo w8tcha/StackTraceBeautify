aad4dd8690fb3f99de03aaa6c199c65fadc2e87a
Ingo <thewatcher@watchersnet.de>
2023-11-04 04:20:21 +0100

    switched to xunit

--- StackTraceBeautify.Tests/BeautifyTests.cs
+++ StackTraceBeautify.Tests/BeautifyTests.cs
@@ -13,11 +13,11 @@
  *
  */
 
-namespace StackTraceBeautify.Tests
-{
-    using NUnit.Framework;
+namespace StackTraceBeautify.Tests;
 
-    /// <summary>
+using Xunit;
+
+/// <summary>
     /// The beautify tests.
     /// </summary>
     public class BeautifyTests
@@ -25,25 +25,26 @@ namespace StackTraceBeautify.Tests
         /// <summary>
         /// Beautify Test with English Stack Trace
         /// </summary>
-        [Test]
+        [Fact]
         public void EnglishTest1()
         {
             const string Expected = "System.FormatException: Input string was not in a correct format.\r\n   at <span class=\"st-frame\"><span class=\"st-type\">System.Number</span>.<span class=\"st-method\">ThrowOverflowOrFormatException</span><span class=\"st-frame-params\">(<span class=\"st-param-type\">ParsingStatus</span> <span class=\"st-param-name\">status</span>, <span class=\"st-param-type\">TypeCode</span> <span class=\"st-param-name\">type</span>)</span></span>\r\n   at <span class=\"st-frame\"><span class=\"st-type\">System.Number</span>.<span class=\"st-method\">ParseInt32</span><span class=\"st-frame-params\">(<span class=\"st-param-type\">ReadOnlySpan`1</span> <span class=\"st-param-name\">value</span>, <span class=\"st-param-type\">NumberStyles</span> <span class=\"st-param-name\">styles</span>, <span class=\"st-param-type\">NumberFormatInfo</span> <span class=\"st-param-name\">info</span>)</span></span>\r\n   at <span class=\"st-frame\"><span class=\"st-type\">System.Int32</span>.<span class=\"st-method\">Parse</span><span class=\"st-frame-params\">(<span class=\"st-param-type\">String</span> <span class=\"st-param-name\">s</span>)</span></span>\r\n   at <span class=\"st-frame\"><span class=\"st-type\">MyNamespace.IntParser</span>.<span class=\"st-method\">Parse</span><span class=\"st-frame-params\">(<span class=\"st-param-type\">String</span> <span class=\"st-param-name\">s</span>)</span></span> in C:\\apps\\MyNamespace\\IntParser.cs:<span class=\"st-line\">line 11</span>\r\n   at <span class=\"st-frame\"><span class=\"st-type\">MyNamespace.Program</span>.<span class=\"st-method\">Main</span><span class=\"st-frame-params\">(<span class=\"st-param-type\">String[]</span> <span class=\"st-param-name\">args</span>)</span></span> in <span class=\"st-file\">C:\\apps\\MyNamespace\\Program.cs</span>:<span class=\"st-line\">line 12</span>";
-            
-            const string Stack = @"System.FormatException: Input string was not in a correct format.
-   at System.Number.ThrowOverflowOrFormatException(ParsingStatus status, TypeCode type)
-   at System.Number.ParseInt32(ReadOnlySpan`1 value, NumberStyles styles, NumberFormatInfo info)
-   at System.Int32.Parse(String s)
-   at MyNamespace.IntParser.Parse(String s) in C:\apps\MyNamespace\IntParser.cs:line 11
-   at MyNamespace.Program.Main(String[] args) in C:\apps\MyNamespace\Program.cs:line 12";
+
+            const string Stack = """
+                                 System.FormatException: Input string was not in a correct format.
+                                    at System.Number.ThrowOverflowOrFormatException(ParsingStatus status, TypeCode type)
+                                    at System.Number.ParseInt32(ReadOnlySpan`1 value, NumberStyles styles, NumberFormatInfo info)
+                                    at System.Int32.Parse(String s)
+                                    at MyNamespace.IntParser.Parse(String s) in C:\apps\MyNamespace\IntParser.cs:line 11
+                                    at MyNamespace.Program.Main(String[] args) in C:\apps\MyNamespace\Program.cs:line 12
+                                 """;
 
             var beautify = new StackTraceBeautify();
 
             var result = beautify.Beautify(Stack);
 
-            Assert.AreEqual("english", beautify.GetLanguage());
+            Assert.Equal("english", beautify.GetLanguage());
 
-            Assert.AreEqual(Expected, result);
+            Assert.Equal(Expected, result);
         }
     }
-}
